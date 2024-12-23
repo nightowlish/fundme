21982facc98e0aa8bbc52d6df2195a4e5c1e77da
nightowlish <oanaolf@gmail.com>
2024-12-23 12:21:52 +0200

    Add priceFeed mock for Anvile deploy

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -4,14 +4,28 @@ pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
 import {Constants} from "../lib/Constants.sol";
+import {MockV3Aggregator} from "mocks/MockV3Aggregator.sol";
 
 contract HelperConfig is Script {
     Constants.NetworkConfig public activePriceFeed;
 
     constructor() {
-        address priceFeedAddress = Constants.chainIdToEthUsdConfig(
-            block.chainid
-        );
-        activePriceFeed = Constants.NetworkConfig(priceFeedAddress);
+        if (block.chainid == Constants.ANVILE_CHAIN_ID) {
+            activePriceFeed = getAnvileEthUsdConfig();
+        } else {
+            activePriceFeed = Constants.chainIdToEthUsdConfig(block.chainid);
+        }
+    }
+
+    function getAnvileEthUsdConfig()
+        public
+        returns (Constants.NetworkConfig memory)
+    {
+        // Deploy mock priceFeed contract
+        vm.startBroadcast();
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(8, 3000e8);
+        vm.stopBroadcast();
+
+        return Constants.NetworkConfig(address(mockPriceFeed));
     }
 }
